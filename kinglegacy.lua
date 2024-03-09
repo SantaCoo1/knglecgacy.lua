a7254ea51157b7087880c020acea125f175dda5f
SantaCoo1 <162719259+SantaCoo1@users.noreply.github.com>
2024-03-09 10:34:33 +0530

    Create kinglegacy.lua

---
+++ kinglegacy.lua
@@ -0,0 +1,17 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	--Blox Fruit
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/bf.lua"))()
+elseif placeId == 4520749081 or placeId == 6381829480 or placeId == 5931540094 or placeId == 15759515082 then
+	--King Legacy
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/kl.lua"))()
+elseif placeId == 6777872443 then
+    	--Pixel Piece
+    	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/pp.lua"))()
+elseif placeId == 12192552089 then
+	--Fruit Warriors
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/fw.lua"))()
+elseif placeId == 6918802270 or placeId == 14979402479 then
+	--HAZE PIECE
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/hp.lua"))()
+end
